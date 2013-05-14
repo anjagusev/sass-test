ab7dc124de2a63f0acf13040d3a58976edb61411
Anja <anja.gusev@gmail.com>
2013-05-13 20:56:11 -0400

    changing styles

--- jquery.js
+++ jquery.js
@@ -55,3 +55,77 @@ $(".cubeblue").click(function() {
 
 });
 
+var fadeStart=100 // 100px scroll or less will equiv to 1 opacity
+	,fadeUntil=700 // 200px scroll or more will equiv to 0 opacity
+	,fading = $('.fading');
+
+$(window).bind('scroll', function(){
+    var offset = $(document).scrollTop()
+        ,opacity=0
+    ;
+    if( offset<=fadeStart ){
+        opacity=1;
+    }else if( offset<=fadeUntil ){
+        opacity=1-offset/fadeUntil;
+    }
+    fading.css('opacity',opacity);
+});
+
+// Smooth ScrollTo Code
+
+$(".scroll").click(function(event){
+	event.preventDefault();
+	//calculate destination place
+	var dest=0;
+	if($(this.hash).offset().top > $(document).height()-$(window).height()){
+		dest=$(document).height()-$(window).height();
+	}else{
+		dest=$(this.hash).offset().top;
+	}
+	//go to destination
+	$('html,body').animate({scrollTop:dest}, 1000,'swing');
+});
+
+
+//$(document).ready(function(){
+	
+//	// Reveal after scrolling  
+//	$(window).bind('scroll', function(){
+//		$(".trigger").toggle($(this).scrollTop() > 200);
+//	});
+//	
+//});
+
+$(window).bind('scroll', function(){
+    if($(this).scrollTop() > 600) {
+        $(".trigger").fadeIn('slow');
+        $(".hideclick").fadeIn('slow');
+    }
+    else if($(this).scrollTop() < 600) {
+    	$(".trigger").hide();
+    }
+});
+
+$(document).ready(function(){        
+    $(".trigger").hide();
+
+	// When p.hide is clicked, run the hideDiv function, use this to target itself		
+	$('.hideclick').click(function()
+	{
+		$.fn.hideDiv(this);
+	});
+	
+	// fade in containing div
+	$("#fading").fadeIn('slow');
+	
+	
+});
+
+// When called, hides all divs using the class name .showhide
+$.fn.hideDiv = function(e)
+{
+	$(e).fadeOut('fast');
+}
+
+
+
