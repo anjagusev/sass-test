571ef704fc0123d1bc5d3af9052a2ffa845d6dd3
Anja <anja.gusev@gmail.com>
2013-05-14 21:57:45 -0400

    remove top triangle

--- js/triangles.js
+++ js/triangles.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
    window.onload = function () {
+ 
             var Paper = Raphael(document.getElementById("triangle"), 400, 400);
           var r = Raphael(document.getElementById("testT"));
  var path = Paper.path("M 0 287 l 159 -287 l 159 287 z");
 
+var w = 1400;
+var h = 600;
+
+r.setViewBox(0,0,w,h,true);
+
+// ok, raphael sets width/height even though a viewBox has been set, so let's rip out those attributes (yes, this will not work for VML)
+var svg = document.querySelector("svg");
+svg.removeAttribute("width");
+svg.removeAttribute("height");
+
+
   //<path d="M0 0 L100 100 L200 0 Z" />
  var pathy = r.path("M 150 0 L314 287 L478 0 z");
   var path3 = r.path("M 800 0 L964 290 L1130 0 z");
-  var y = r.path("M 910 424 L374 424 L638 -50 z");
+  var y = r.path("M 910 424 L374 424 L638 0 z");
 path.attr({fill: 'white'});
 path3.attr({fill: 'black'});
-pathy.attr({fill:'url("apple.png")'});
+pathy.attr({fill:'green'});
 y.attr({fill:'url("apple.png")'});
 
 r.print(300, 100, "Test string", r.getFont("Times", 800), 30);
